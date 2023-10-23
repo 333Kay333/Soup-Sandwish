85eabf7bfd25e6adf986a1d27dc4c39f42c3e038
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-10-23 10:27:43 -0400

    #parents and children

--- src/index.js
+++ src/index.js
@@ -3,25 +3,48 @@ const rootElement = document.getElementById('root');
         const element = <div></div>
         ReactDOM.render(element, rootElement);
 
-
-function TempName() {
-  return (
-<div>
-  <header>
+        function Header() {
+          return(<div>
+             <header>
     <nav>
     <img src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png" width="40px" />
     </nav>
   </header>
-<h1>Fun Facts About Kay</h1>
+          </div>
+            
+          )
+        };
+        function MainContent() {
+          return(
+            <div>
+            <h1>Fun Facts About Kay</h1>
 
     <ul>
       <li>I have lived in some very interesting places. P.N.G., Buckland, Girdwood, etc</li>
       <li>I have a wide variety of certificates and licenses including but not limited to a Pilots License, Cosmetology Licenses in multiple states, Realtor License...</li>
       <li>I love to cook.  Transforming food is one of the most fun things I can think of.</li>
-     </ul>
-     <footer>© 20xx <Kay's Coding> development. All rights reserved."
+     </ul></div>
+          )
+        };
+        function Footer() {
+          return(
+            <div>
+              <footer>
+      <small>© 20xx Kay's Coding development. All rights reserved.</small>
 </footer>
+            </div>
+
+          )
+        }
+
+function TempName() {
+  return (
+<div>
+  <Header />
+<MainContent />
+     <Footer />
     </div> )
 };
+
 
 ReactDOM.render(<TempName />, document.getElementById('root'));
