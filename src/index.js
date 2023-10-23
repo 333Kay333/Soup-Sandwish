61149ab98c904642b0f0b0666620b73aab27ba35
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-10-23 09:49:00 -0400

    #react 2nd project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+const rootElement = document.getElementById('root');
+        const element = <div></div>
+        ReactDOM.render(element, rootElement);
+
+
+const page = (
+<div>
+  <div>
+    <img src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png" width="40px" />
+  </div>
+<h1>Fun Facts About Kay</h1>
+
+    <ul>
+      <li>I have lived in some very interesting places. P.N.G., Buckland, Girdwood, etc</li>
+      <li>I have a wide variety of certificates and licenses including but not limited to a Pilots License, Cosmetology Licenses in multiple states, Realtor License...</li>
+      <li>I love to cook.  Transforming food is one of the most fun things I can think of.</li>
+    </ul>
+    </div> 
+);
+ReactDOM.render(page, document.getElementById('root'));
