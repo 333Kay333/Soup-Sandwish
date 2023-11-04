8e76a2d76081c88dd6c14cc2244d718f1b0666ef
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-11-04 11:15:01 -0400

    #some hyperlinks and html files

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ const rootElement = document.getElementById('root');
       </header>
    
     <ul className="nav-item">
-    <li>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
+      <li><a href="./menu.html">Menu</a></li>
+      <li><a href="./about.html">About</a></li>
+      <li><a href="./contact.html">Contact</a></li>
      </ul>
     </nav>
     <h1>Soup & Wishes</h1></div>
